2ffe94102a2b9e7f75e3be3a17cc711a795910df
Shub <18.shubhangee.das@gmail.com>
2024-10-23 18:01:09 +0100

    updated buttons

--- RoboRally/mainMenuController.py
+++ RoboRally/mainMenuController.py
@@ -1,25 +1,62 @@
-import tkinter 
+from tkinter import * 
 from tkinter import ttk 
-import tkinter.messagebox
+from tkinter.ttk import * 
 
 class MainMenuController: 
     def __init__(self, model, view):
         self.model = model 
         self.view = view 
 
+    # so wehn user clicks, open a new window for each of these 
     def handleClick(self,action):
         if action == 'Tutorial':
-            self.Tutorial() 
-        elif action == 'SpVBOT':
-            self.spVsBot() 
-        elif action == 'Leaderboard': 
-            self.viewLeaderboard() 
+            self.tutorialWindow() 
+        elif action == 'vsBot':
+            self.vsBotWindow() 
+        elif action == 'Multiplayer': 
+            self.multiplayerWindow() 
+        elif action == 'Leaderboard':
+            self.leaderboardWindow() 
+        elif action == 'LdSaved':
+            self.ldSavedWindow()
+        elif action == 'Rules':
+            self.rulesWindow() 
     
-    def Tutorial(self):
-        print('tutorial button works!')
+    def tutorialWindow(self):
+        tutorialWindow = Toplevel(self.view.root)
+        tutorialWindow.title('Tutorial')
+        tutorialWindow.geometry('400x300')
+        label = ttk.Label(tutorialWindow,text='Welcome to the Tutorial')
+        label.pack(pady=20)
 
-    def spVsBot(self):
-        print('SpVS BOT works!')
+    def vsBotWindow(self):
+       vsBotWindow = Toplevel(self.view.root)
+       vsBotWindow.title('Single Player vs Bot')
+       vsBotWindow.geometry('400x300')
+       label = ttk.Label(vsBotWindow, text='Single Player vs Bot') 
+       label.pack(pady=20)
 
-    def viewLeaderboard(self):
-        print('leaderboard works!')
+    def multiplayerWindow(self):
+        multiplayerWindow = Toplevel(self.view.root)
+        multiplayerWindow.title('Multiplayer Mode')
+        multiplayerWindow.geometry('400x300')
+        label=ttk.Label(multiplayerWindow, text='Welcome to multiplayer game')
+        label.pack(pad=20)
+
+    def leaderboardWindow(self):
+        leaderboardWindow = Toplevel(self.view.root)
+        leaderboardWindow.title('Leaderboard')
+        leaderboardWindow.geometry('400x300')
+        # insert some leaderboard logic including sorting of leaderboard, read from an external file 
+
+    def ldSavedWindow(self):
+        # include some logic for saving game and maybe a message like 'sure you wanna save this thing' 
+        pass 
+        print('saving game button works!')
+
+    def rulesWindow(self):
+        rulesWindow = Toplevel(self.view.root)
+        rulesWindow.title('Robo Rally Rules')
+        rulesWindow.geometry('400x300')
+        label1 = ttk.Label(rulesWindow, text='Rules here...')
+        label1.pack(pady=20)
--- RoboRally/mainMenuView.py
+++ RoboRally/mainMenuView.py
@@ -7,24 +7,34 @@ class MainMenuView:
         self.controller = controller  
         self.root = tk.Tk() 
         self.root.title('Main Menu')
-        self.root.geometry('300x300')
-        self.root.minsize(300,300)
+        self.root.geometry('400x400')
+        self.root.minsize(400,400)
 
         self.label = ttk.Label(self.root, text='This is the Main Menu')
         self.label.pack(pady=20)
         
         self.buttons = []
+        
         self.tutorialBtn = ttk.Button(self.root, 
                                       text='Tutorial', 
                                       command=lambda: self.controller.handleClick('Tutorial'))
-        self.vsBot = ttk.Button(self.root, 
-                                text='SP vs Bot', 
-                                command=lambda: self.controller.handleClick('SpVBOT'))
+        self.vsBotBtn = ttk.Button(self.root, 
+                                text='Single Player vs Bot', 
+                                command=lambda: self.controller.handleClick('vsBot'))
+        self.multiplayerBtn = ttk.Button(self.root, 
+                                      text='Multiplayer', 
+                                      command=lambda: self.controller.handleClick('Multiplayer'))
         self.LeaderboardBtn = ttk.Button(self.root, 
-                                        text='Leaderboard', 
+                                        text='View Leaderboard', 
                                         command=lambda: self.controller.handleClick('Leaderboard'))
-        
-        self.buttons=[self.tutorialBtn, self.vsBot, self.LeaderboardBtn]
+        self.LdSavedBtn = ttk.Button(self.root, 
+                                  text='Load Saved', 
+                                  command=lambda: self.controller.handleClick('LdSaved'))
+        self.rulesBtn= ttk.Button(self.root, 
+                                text='View Robo Rally Rules', 
+                                command=lambda: self.controller.handleClick('Rules'))
+
+        self.buttons=[self.tutorialBtn, self.vsBotBtn, self.multiplayerBtn, self.LeaderboardBtn, self.LdSavedBtn, self.rulesBtn]
 
         for button in self.buttons: 
             button.pack(pady=10)
